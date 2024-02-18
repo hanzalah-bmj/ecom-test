c9e0ef7a6101b66faaaa866b24ba6d8e97087518
hanzalah-bmj <hanzalah.bmj@gmail.com>
2024-02-18 22:51:59 +0500

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Navbar from './components/Navbar';
+import { Footer } from './components/Footer';
+import Home from './components/Home';
+import Catalog from './components/Catalog';
+import About from './components/About';
+import Contact from './components/Contact';
+import Cart from './components/Cart';
+import AdminDashboard from './components/admin/AdminDashboard';
+import UserDashboard from './components/users/UserDashboard';
+import Signin from './components/Signin';
+import { ErrorPage } from './components/ErrorPage';
 
-function App() {
+const DefaultLayout = ({ children }) => (
+  <>
+    <Header />
+    <Navbar />
+    {children}
+    <Footer />
+  </>
+);
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Routes>
+        <Route path="/" element={<DefaultLayout><Home /></DefaultLayout>} />
+        <Route path="/catalog" element={<DefaultLayout><Catalog /></DefaultLayout>} />
+        <Route path="/about" element={<DefaultLayout><About /></DefaultLayout>} />
+        <Route path="/contact" element={<DefaultLayout><Contact /></DefaultLayout>} />
+        <Route path="/cart" element={<DefaultLayout><Cart /></DefaultLayout>} />
+        <Route path="/signin" element={<DefaultLayout><Signin /></DefaultLayout>} />
+        <Route path="*" element={<DefaultLayout><ErrorPage /></DefaultLayout>} />
+        {/* User Dashboard */}
+        <Route path="/users/*" element={<UserDashboard />} />
+
+        {/* Admin Dashboard */}
+        <Route path="/admin/*" element={<AdminDashboard />} />
+        
+      </Routes>
   );
 }
-
-export default App;
